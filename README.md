3d38e2e31bf2518ebde9a7d21c1d92f03c4cb61c
Spandan <dhungelspandan@gmail.com>
2017-01-19 14:04:24 +0545

    added

---
+++ README.md
@@ -0,0 +1,24 @@
+# [Start Bootstrap](http://listthemes.com/) - [Creative](http://listthemes.com/template-overviews/creative/)
+
+[Creative](http://listthemes.com/template-overviews/creative/) is a one page creative theme for [Bootstrap](http://getbootstrap.com/) created by [List Themes](http://listthemes.com/).
+
+## Getting Started
+
+To begin using this template, choose one of the following options to get started:
+
+
+
+## Bugs and Issues
+
+
+## Creator
+
+Listthemes
+
+## Creator
+
+
+
+## Copyright and License
+
+Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/listthemes-creative/blob/gh-pages/LICENSE) license.
---
+++ footer.php
@@ -0,0 +1,37 @@
+ <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="<?php bloginfo('template_directory'); ?>/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php bloginfo('template_directory'); ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
